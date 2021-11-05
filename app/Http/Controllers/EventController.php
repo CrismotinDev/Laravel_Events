ea3da3a3473a1470c00b6ebce73b6806d454e593
cristiane <cristiane.motin@rpinfo.com.br>
2021-11-05 15:32:20 -0300

    incluindo a opcao manyTomany

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -93,4 +93,20 @@ class EventController extends Controller
         return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
 
     }
-}
+
+    public function joinEvent($id){
+
+        $user = auth()->user();
+        
+        $user->eventsAsParticipant()->attach($id);
+        
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença foi confirmada no evento ' . $event->title);
+
+
+        }
+    }
+
+
