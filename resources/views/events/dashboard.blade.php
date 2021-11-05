b3a970a5841c855cf049f7d7e6f7ccd93e95e7b0
cristiane <cristiane.motin@rpinfo.com.br>
2021-11-05 12:32:35 -0300

    Incluindo o botao excluir evento

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -26,8 +26,16 @@
                             <th scropt="row"> {{ $loop->index + 1 }} </th>
                             <td><a href="/events/{{ $event->id }}"> {{ $event->title }}</a>
                             <td>0</td>
-                            <td><a href="#">Editar </a> <a href="#">Deletar</a></td>
-                        </tr>
+                            <td>
+                                <a href="#" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon>Editar </a> 
+                               <form action="/events/{{$event->id}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon>Deletar</button>
+
+                            </form>
+                            </td>
+                            </tr>
                     @endforeach
                 </tbody>
             </table>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', [EventController::class, 'index']); // index mostra os registros
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth'); // para mostrar o formulario para criar no banco
 Route::get('/events/{id}', [EventController::class, 'show']); // mostrar um dado especifico
 Route::post('events', [EventController::class, 'store']);//enviar os dados para o banco
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
 
 
 Route::get('/contact', function () {
