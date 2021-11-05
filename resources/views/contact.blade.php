f796b16e656d5cfc9f6bafb4ceb2d47799b0bb1d
cristiane <cristiane.motin@rpinfo.com.br>
2021-11-05 11:00:37 -0300

    Eventos

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Ola bem vindo ao laravel </h1>
+<a href="/">Voltar para home</a>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']); // index mostra os registros
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth'); // para mostrar o formulario para criar no banco
+Route::get('/events/{id}', [EventController::class, 'show']); // mostrar um dado especifico
+
+Route::post('events', [EventController::class, 'store']);//enviar os dados para o banco
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
