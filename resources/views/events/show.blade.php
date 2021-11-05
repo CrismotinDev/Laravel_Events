ce911e331f23974f24221669d8ae405f64074e6a
cristiane <cristiane.motin@rpinfo.com.br>
2021-11-05 18:09:28 -0300

    Finalizando Site Eventos

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -16,25 +16,31 @@
                     <ion-icon class="location-outline"></ion-icon>{{ $event->city }}
                 </p>
                 <p class="events-participants">
-                    <ion-icon name="people-outline"></ion-icon> {{ count ($event->users) }} Participantes
+                    <ion-icon name="people-outline"></ion-icon> {{ count($event->users) }} Participantes
                 </p>
                 <p class="event-owner">
-                    <ion-icon name="star-outline"></ion-icon>{{ $eventOwner['name']}} </p>
-                <form action="/events/join/{{ $event->id }}" method="POST">
-                @csrf
-                <a href="/events/join/{{ $event->id}}"
-                     class="btn btn-primary" 
-                     id="event-submit"
-                     onclick="event.preventDefault();
-                     this.closest('form').submit();">
-                     Confirmar presença
-                    </a>
-                </form>
+                    <ion-icon name="star-outline"></ion-icon>{{ $eventOwner['name'] }}
+                </p>
+                @if (!$hasUserJoined)
+                    <form action="/events/join/{{ $event->id }}" method="POST">
+                        @csrf
+                        <a href="/events/join/{{ $event->id }}" class="btn btn-primary" id="event-submit" onclick="event.preventDefault();
+                                 this.closest('form').submit();">
+                            Confirmar presença
+                        </a>
+                    </form>
+
+                @else
+                <p class="already-joined-msg">Você já está participando deste evento!</p>
+
+                @endif
                 <h3>O evento conta com:</h3>
                 <ul id="items-list">
-                    @foreach ($event->items as  $item) 
-                        <li><ion-icon name="play-outline"></ion-icon> <span> {{ $item }}</span></li>
-                        
+                    @foreach ($event->items as $item)
+                        <li>
+                            <ion-icon name="play-outline"></ion-icon> <span> {{ $item }}</span>
+                        </li>
+
                     @endforeach
                 </ul>
             </div>
